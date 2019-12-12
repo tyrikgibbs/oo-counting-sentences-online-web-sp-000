1962f87e46fdab812cebc73508d3fafc62b89d40
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-12 01:12:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-  if  self.end_with?(".")
-    return true
-  else
-    return false
-  end
+    if  self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if  self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
